6fe80ce9c9607088006a48aa407e9f9b6d872238
ildarVagapov <90469542+ildarVagapov@users.noreply.github.com>
2022-05-22 22:28:13 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$ch = curl_init('http://localhost:3000/index2.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HEADER, false);
+$html = curl_exec($ch);
+curl_close($ch);
+ 
+echo $html;
+?>
